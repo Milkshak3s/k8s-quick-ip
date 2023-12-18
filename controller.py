3ebc4c882bf1e899dd2855100e82f53f612800a1
Milkshak3s <justchris.vantine@gmail.com>
2023-12-18 13:09:44 -0800

    Add delete functionality

--- controller.py
+++ controller.py
@@ -14,6 +14,10 @@ def check_spec_fields(field_names, spec):
         spec_value = spec.get(name)
         if spec_value is None:
             raise kopf.HandlerFatalError(f"{name} must be set. Got {spec_value!r}.")
+        
+
+def generate_client_deploy_name(name):
+    return f"frpc-{name}"
 
 
 @kopf.on.create('experimental.milkshakes.cloud', 'v1', 'frpgcpremotes')
@@ -22,7 +26,7 @@ def create_fn(meta, spec, namespace, logger, body, **kwargs):
     check_spec_fields(fields, spec)
 
     remote_job_name = f"deployremote-{meta.get('name')}"
-    client_deploy_name = f"frpc-{meta.get('name')}"
+    client_deploy_name = generate_client_deploy_name(meta.get('name'))
 
     gcp_project_id = spec.get('gcp_project_id')
     dns_a_name = spec.get('dns_a_name')
@@ -34,11 +38,12 @@ def create_fn(meta, spec, namespace, logger, body, **kwargs):
 
     path = os.path.join(os.path.dirname(__file__), 'remote-job.yaml')
     tmpl = open(path, 'rt').read()
-    text = tmpl.format(name=remote_job_name, gcp_project_id=gcp_project_id, dns_a_name=dns_a_name, dns_zone=dns_zone, dns_base_domain=dns_base_domain, frp_server_port=frp_server_port, gcp_service_account_secret=gcp_service_account_secret)
+    text = tmpl.format(name=remote_job_name, gcp_project_id=gcp_project_id, dns_a_name=dns_a_name, dns_zone=dns_zone, dns_base_domain=dns_base_domain, frp_server_port=frp_server_port, gcp_service_account_secret=gcp_service_account_secret, delete_not_create="false")
     data = yaml.load(text, yaml.Loader)
 
     kopf.adopt(data, owner=body)
 
+    # run job to deploy remote GCP resources
     batch_api = kubernetes.client.BatchV1Api()
     obj = batch_api.create_namespaced_job(
         namespace=namespace,
@@ -59,10 +64,45 @@ def create_fn(meta, spec, namespace, logger, body, **kwargs):
 
     kopf.adopt(data, owner=body)
 
+    # run local client deployment
     apps_api = kubernetes.client.AppsV1Api()
     obj = apps_api.create_namespaced_deployment(
         namespace=namespace,
         body=data,
     )
 
-    return {'FrpGCPRemote-name': obj.metadata.name}
+    return {'FrpGCPRemote-job-name': obj.metadata.name}
+
+
+@kopf.on.delete('experimental.milkshakes.cloud', 'v1', 'frpgcpremotes')
+def delete_fn(body, meta, spec, status, **kwargs):
+    # run job to delete remote GCP resources
+    remote_job_name = f"deleteremote-{meta.get('name')}"
+    gcp_project_id = spec.get('gcp_project_id')
+    dns_a_name = spec.get('dns_a_name')
+    dns_zone = spec.get('dns_zone')
+    dns_base_domain = spec.get('dns_base_domain')
+    frp_server_port = spec.get('frp_server_port')
+    gcp_service_account_secret = spec.get('gcp_service_account_secret')
+
+
+    path = os.path.join(os.path.dirname(__file__), 'remote-job.yaml')
+    tmpl = open(path, 'rt').read()
+    text = tmpl.format(name=remote_job_name, gcp_project_id=gcp_project_id, dns_a_name=dns_a_name, dns_zone=dns_zone, dns_base_domain=dns_base_domain, frp_server_port=frp_server_port, gcp_service_account_secret=gcp_service_account_secret, delete_not_create="true")
+    data = yaml.load(text, yaml.Loader)
+
+    # kopf.adopt(data, owner=body)
+
+    # run job to deploy remote GCP resources
+    namespace = meta.get('namespace')
+    batch_api = kubernetes.client.BatchV1Api()
+    obj = batch_api.create_namespaced_job(
+        namespace=namespace,
+        body=data,
+    )
+
+    # delete frpc client deployment
+    apps_api = kubernetes.client.AppsV1Api()
+    apps_api.delete_namespaced_deployment(name=generate_client_deploy_name(meta.get('name')), namespace=namespace)
+
+    return {'FrpGCPRemote-delete-job-name': obj.metadata.name}
